45ee203f7876ebba969fe72002c2e23d50adcb4c
Felipe Azevedo <felipe.azevedoit@outlook.com>
2024-08-07 13:05:23 -0300

    Feature --> authenticaqtion

---
+++ src/ConsultEase.PatientService.Core.Domain/Interfaces/IPatientRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PatientService.Domain.Models;
+
+namespace PatientService.Domain.Interfaces
+{
+    public interface IPatientRepository
+    {
+        Task<Patient> GetByIdAsync(int id);
+        Task<IEnumerable<Patient>> GetAllAsync();
+        Task AddAsync(Patient patient);
+        Task UpdateAsync(Patient patient);
+        Task DeleteAsync(int id);
+    }
+}
